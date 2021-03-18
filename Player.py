e0ffd66a574c732d1f5969f46e5e5b4c4611cc47
MikulasIbl <mikulas.ibl@pslib.cz>
2021-03-18 20:25:15 +0100

    snad

--- Player.py
+++ Player.py
@@ -1,4 +1,5 @@
-# By Pytel
+# By Pytel, Guly
+from prompt_toolkit import prompt
 """
 Funkce hrace pro hru 2048.
 """
@@ -12,7 +13,7 @@ class Player:
 	
 	def Move (self):
 		while True != False:
-			key = input()
+			key = prompt()
 			if DEBUG:
 				print(key)
 			if key in MOVE:
@@ -22,4 +23,8 @@ class Player:
 
 """
 END
+from prompt_toolkit import prompt
+ 
+cmd = prompt("Command: ")
+print("Entered: '{cmd}'".format(cmd=cmd))
 """
